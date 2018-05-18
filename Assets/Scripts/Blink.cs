48543d6c729aa033555136dd27be1f5b06160d20
Xavicity <xavicityx@gmail.com>
2018-05-18 22:13:57 +0200

    Fade In and Out basico

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+
+	bool isShowing;
+	bool isInTransition;
+	float duration;
+	float transition;
+	Material thisMaterial;
+	// Use this for initialization
+void Start(){
+		thisMaterial=this.GetComponent<MeshRenderer>().material;
+		thisMaterial.color=new Color(1,1,1,0);
+		
+	}
+	public void doFadeWhenGetHit(){
+		Fade(true,0.4f);
+	}
+	public void Fade(bool showing,float duration){
+		isShowing=showing;
+		isInTransition=true;
+		this.duration=duration;
+		transition=(isShowing) ? 0 : 1;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.E)){
+			doFadeWhenGetHit();
+		}
+		if(!isInTransition){
+			thisMaterial.color=new Color(1,1,1,0);
+			return;
+		}
+		
+		transition+= (isShowing) ? Time.deltaTime*(1/duration) : -Time.deltaTime*(1/duration);
+		thisMaterial.color=Color.Lerp(thisMaterial.color,new Color(1,1,1,1),transition);
+
+		if(transition>1 || transition<0){
+			isInTransition=false;
+			isShowing=false;
+		}
+	}
+}
---
+++ Assets/Scripts/PoolParticulasPared.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolParticulasPared : MonoBehaviour {
+
+	private int particleDecalDataIndex;
+	public int
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void setParticleData(ParticleCollisionEvent particleCollisionEvent){
+		//record collision,position,rotation,size and color
+		if(particleDecalDataIndex>)
+	}
+}

75fd0d7b2742a95bea0a7aa704835314324e9dee
Xavicity <xavicityx@gmail.com>
2018-05-19 20:36:29 +0200

    Arreglada puerta sin fisicas

--- Assets/Scripts/Portazo.cs
+++ Assets/Scripts/Portazo.cs
@@ -6,10 +6,12 @@ public class Portazo : MonoBehaviour {
 
 	bool opened;
 	bool free;
+	bool done;
 	// Use this for initialization
 	void Start () {
 		opened=false;
 		free=false;
+		done=false;
 	}
 	
 	// Update is called once per frame
@@ -17,25 +19,30 @@ public class Portazo : MonoBehaviour {
 		opened=true;
 	}
 	void Update(){
-		if(opened && !free){
+		if(opened && !free && !done){
 			Color c=this.GetComponent<MeshRenderer>().material.color;
 			this.GetComponent<MeshRenderer>().material.color=new Color(c.r,c.b,c.g,0.6f);
 			this.transform.Rotate(0,3,0);
-			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotationY;
 				
-		}else{
+		}else if(!opened && free){
 			this.gameObject.GetComponent<Rigidbody>().velocity=Vector3.zero;
-			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.None;
-			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePosition;
+			done=true;
+			free=false;
+			this.transform.Rotate(0,-30,0);
 		}
-		if(free){
-			
+		if(done ){
+			Debug.Log("sdfsdf");
+			this.GetComponent<MeshRenderer>().material.color=GameObject.Find("filing-cabinet-empty (3)").GetComponent<MeshRenderer>().material.color;
+			if(this.GetComponent<MeshRenderer>().material.color.a<0.1f){
+				done=false;
+			}
 		}
 	}
 
 	void OnCollisionEnter(Collision e){
 		if(e.gameObject.name=="filing-cabinet-empty (3)"){
 			free=true;
+			opened=false;
 		}
 	}
 	

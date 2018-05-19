b02e52e4b9a97474109ba41d906647e1e3f5f6d0
Raed93 <raeday93@gmail.com>
2018-05-19 16:49:13 +0200

    Habitacion1_Completada

---
+++ Assets/Scripts/fan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Color c=this.getComponent<Renderer>().Material.Color ;
+		//this.getComponent<Renderer>().Material.Color= new Color(c.r,c.g,c.b,)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

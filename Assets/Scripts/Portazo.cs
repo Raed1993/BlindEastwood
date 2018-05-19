6c922f7be1dc4260d8ed05e52fc2e03a4260e381
Raed93 <raeday93@gmail.com>
2018-05-19 19:34:16 +0200

    Portazo script

--- Assets/Scripts/Portazo.cs
+++ Assets/Scripts/Portazo.cs
@@ -21,9 +21,12 @@ public class Portazo : MonoBehaviour {
 			Color c=this.GetComponent<MeshRenderer>().material.color;
 			this.GetComponent<MeshRenderer>().material.color=new Color(c.r,c.b,c.g,0.6f);
 			this.transform.Rotate(0,3,0);
+			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotationY;
 				
 		}else{
 			this.gameObject.GetComponent<Rigidbody>().velocity=Vector3.zero;
+			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.None;
+			this.gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePosition;
 		}
 		if(free){
 			

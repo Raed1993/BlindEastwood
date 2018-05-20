855281406f9ac712c6956b84a639867c9ecdc8c5
Xavicity <xavicityx@gmail.com>
2018-05-20 10:13:25 +0200

    Blink Multimaterial hecho, en principio

---
+++ Assets/Scripts/BlinkMultimaterial.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkMultimaterial : MonoBehaviour {
+
+    public float duration = 0.1f;
+    public float transitionIn=0.1f;
+    public float transitionOut=0.01f;
+
+    bool isShowing;
+	bool isInTransition;
+    float alphaFade;
+    float transition;
+	Material[] thisMaterials;
+	// Use this for initialization
+    void Start()
+    {
+		thisMaterials=this.GetComponent<MeshRenderer>().materials;
+        foreach(Material m in thisMaterials)
+		    m.color=new Color(1,1,1,0);
+		
+	}
+
+	void OnTriggerEnter(Collider e){
+        transition = transitionIn;
+        alphaFade = 0.6f;
+        Debug.Log("hola");
+		if(e.gameObject.tag=="Player" || e.gameObject.tag=="Moneda" || e.gameObject.tag=="Moneda" || e.gameObject.tag=="Door"){
+            InvokeRepeating("Fade", duration, duration);
+        }
+	}
+
+    void OnTriggerExit(Collider e)
+    {
+        transition = transitionOut;
+        alphaFade = 0;
+        Debug.Log("adios");
+        if (e.gameObject.tag == "Player"|| e.gameObject.tag=="Moneda" || e.gameObject.tag=="Moneda" || e.gameObject.tag=="Door")
+        {
+            InvokeRepeating("Fade", duration, duration);
+        }
+    }
+
+    void Fade()
+    {   
+        foreach(Material m in thisMaterials){
+            m.color = Color.Lerp(m.color, new Color(1, 1, 1, alphaFade), transition);
+            if (m.color.a == alphaFade)
+            {
+                CancelInvoke();
+            }
+        }
+    }
+}

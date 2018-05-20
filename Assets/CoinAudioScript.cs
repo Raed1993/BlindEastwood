1c9f56f2e9c96ffee009e5155f305b17648571ac
Xavicity <xavicityx@gmail.com>
2018-05-20 16:28:29 +0200

    MonedaSonidos

---
+++ Assets/CoinAudioScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAudioScript : MonoBehaviour {
+
+	private AudioSource source;
+	public AudioClip woosh;
+	public AudioClip clinck;
+
+	private bool touched;
+
+	bool throwed;
+	// Use this for initialization
+	void Start () {
+		touched=false;
+		throwed=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((this.GetComponent<Rigidbody>().velocity.x>0 || this.GetComponent<Rigidbody>().velocity.y>0 || this.GetComponent<Rigidbody>().velocity.z>0 )&& !throwed){
+				source.clip=woosh;
+				source.PlayOneShot(woosh);
+				throwed=true;
+		}
+		if((this.GetComponent<Rigidbody>().velocity.x>0 || this.GetComponent<Rigidbody>().velocity.y>0 || this.GetComponent<Rigidbody>().velocity.z>0 )&& !throwed && !touched){
+				source.clip=woosh;
+				touched=true;
+		}
+	}
+
+	void OnTriggerEnter(Collider e){
+		if(e.gameObject.tag=="Ground" ){
+			source.clip=clinck;
+		}
+		source.PlayOneShot(clinck);
+	}
+}
